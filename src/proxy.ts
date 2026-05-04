548a597dd3499b1c08d57cc08f71dd6bd4e3e703
Nish323 <doragon.nini@icloud.com>
2026-05-04 23:11:13 +0900

    [fix] proxyの変更

--- src/proxy.ts
+++ src/proxy.ts
@@ -21,9 +21,6 @@ export const config = {
 		'/dashboard/:path*',
 		'/admin/:path*',
 
-		//複数のパスを配列で指定
-		['/profile', '/settings' ,'/account'],
-
 		//除外パターン（APIルートや静的ファイルの場合は発火しない）
 		'/((?!api|_next/static|_next/image|favicon.ico).*)',
 
